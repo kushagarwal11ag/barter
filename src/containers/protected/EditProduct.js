78e71e35016bcc973f0dac031fea9c1f6c7e6cb1
Kushal <kushagarwal11ag@gmail.com>
2024-05-30 23:15:17 +0530

    profile page fix

--- src/containers/protected/EditProduct.js
+++ src/containers/protected/EditProduct.js
@@ -46,7 +46,7 @@ const EditProduct = ({ productId }) => {
 				barterDescription: productDetails.barterDescription || "",
 				price: productDetails.price || 0,
 				meetingSpot: productDetails.meetingSpot || "",
-				isAvailable: productDetails.isAvailable || true,
+				isAvailable: productDetails.isAvailable,
 			});
 
 			setImageURL(productDetails.image);
@@ -277,7 +277,7 @@ const EditProduct = ({ productId }) => {
 												<select
 													className="w-full mt-2 px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-indigo-600 shadow-sm rounded-lg"
 													name="isAvailable"
-													defaultValue={
+													value={
 														credentials.isAvailable
 													}
 													onChange={(e) => {
--- src/containers/protected/Profile.js
+++ src/containers/protected/Profile.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 
 import defaultProfile from "../../../public/defaultProfile.svg";
+import uploadFile from "../../../public/uploadFile.svg";
 
 let toastId;
 const defaultCredentials = {
@@ -25,8 +26,14 @@ const Profile = () => {
 
 	const [credentials, setCredentials] = useState(defaultCredentials);
 	const [formStatus, setFormStatus] = useState("");
-	const [userFile, setUserFile] = useState(null);
-	const [imageUrl, setImageUrl] = useState(defaultProfile);
+	const [userFile, setUserFile] = useState({
+		avatar: null,
+		banner: null,
+	});
+	const [imageUrl, setImageUrl] = useState({
+		avatar: defaultProfile,
+		banner: uploadFile,
+	});
 
 	useEffect(() => {
 		const fetchUser = async () => {
@@ -41,9 +48,15 @@ const Profile = () => {
 				email: userDetails?.email,
 				bio: userDetails?.bio || "",
 				phone: userDetails?.phone || "",
+				displayEmail: userDetails?.displayEmail,
+				displayPhone: userDetails?.displayPhone,
 			});
 
-			setImageUrl(userDetails.avatar?.url || defaultProfile);
+			setImageUrl((prev) => ({
+				...prev,
+				avatar: userDetails?.avatar?.url || defaultProfile,
+				banner: userDetails?.banner?.url || uploadFile,
+			}));
 		};
 		fetchUser();
 	}, []);
@@ -57,13 +70,14 @@ const Profile = () => {
 
 	const handleFileChange = (event) => {
 		const file = event.target.files[0];
-		setUserFile(file);
-		setImageUrl(URL.createObjectURL(file));
-	};
-
-	const cancelForm = () => {
-		setCredentials(defaultCredentials);
-		router.push("/explore");
+		setUserFile((prev) => ({
+			...prev,
+			[event.target.name]: file,
+		}));
+		setImageUrl((prev) => ({
+			...prev,
+			[event.target.name]: URL.createObjectURL(file),
+		}));
 	};
 
 	const handleSubmit = async (event) => {
@@ -72,6 +86,8 @@ const Profile = () => {
 		try {
 			const info = await updatePersonalInfo();
 			if (!info) return;
+			const pwd = await changePassword();
+			if (!pwd) return;
 			const updateFile = await updateFiles();
 			if (!updateFile) return;
 
@@ -80,7 +96,10 @@ const Profile = () => {
 			});
 
 			setCredentials(defaultCredentials);
-			setUserFile(null);
+			setUserFile({
+				avatar: null,
+				banner: null,
+			});
 			setFormStatus("");
 			router.push("/explore");
 		} catch (error) {
@@ -92,7 +111,7 @@ const Profile = () => {
 		const { name, bio, phone, displayEmail, displayPhone } = credentials;
 		try {
 			await axios.put(
-				"/api/v1/users/user/",
+				"/api/v1/users/",
 				{ name, bio, phone, displayEmail, displayPhone },
 				{ withCredentials: true }
 			);
@@ -103,12 +122,48 @@ const Profile = () => {
 		}
 	};
 
+	const changePassword = async () => {
+		const { oldPassword, newPassword, confirmNewPassword } = credentials;
+		if (oldPassword && newPassword) {
+			if (!confirmNewPassword || newPassword !== confirmNewPassword) {
+				setFormStatus("New password mismatch");
+				toast.error("Error", {
+					id: toastId,
+				});
+				return false;
+			}
+			try {
+				await axios.patch(
+					"/api/v1/users/password",
+					{ oldPassword, newPassword },
+					{ withCredentials: true }
+				);
+				return true;
+			} catch (error) {
+				handleAxiosError(error);
+				return false;
+			}
+		} else if (oldPassword || newPassword || confirmNewPassword) {
+			setFormStatus("Must fill all password details to update");
+			toast.error("Error", {
+				id: toastId,
+			});
+			return false;
+		}
+		return true;
+	};
+
 	const updateFiles = async () => {
 		try {
-			if (userFile) {
+			if (userFile.avatar || userFile.banner) {
 				const formData = new FormData();
-				formData.append("avatar", userFile);
-				await axios.patch("/api/v1/users/user/", formData, {
+				if (userFile.avatar) {
+					formData.append("avatar", userFile.avatar);
+				}
+				if (userFile.banner) {
+					formData.append("banner", userFile.banner);
+				}
+				await axios.patch("/api/v1/users/", formData, {
 					withCredentials: true,
 					headers: {
 						"Content-Type": "multipart/form-data",
@@ -131,6 +186,15 @@ const Profile = () => {
 		});
 	};
 
+	const cancelForm = () => {
+		setCredentials(defaultCredentials);
+		setImageUrl({
+			avatar: defaultProfile,
+			banner: uploadFile,
+		});
+		router.push("/explore");
+	};
+
 	return (
 		<>
 			<Toaster />
@@ -142,7 +206,7 @@ const Profile = () => {
 							<p>Please fill out all the fields.</p>
 							<Image
 								name="avatar"
-								src={imageUrl}
+								src={imageUrl.avatar}
 								alt="User Profile Image"
 								width={200}
 								height={200}
@@ -154,6 +218,24 @@ const Profile = () => {
 							<input
 								type="file"
 								name="avatar"
+								accept="image/png, image/jpg, image/jpeg, image/svg"
+								className="mt-3 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+								onChange={handleFileChange}
+							/>
+							<Image
+								name="banner"
+								src={imageUrl.banner}
+								alt="User Banner - Background Image"
+								width={200}
+								height={200}
+								className="w-52 h-52 m-7 rounded-full object-cover object-contain"
+							/>
+							<label className="flex gap-2 items-center justify-start">
+								<p>Upload Background Image</p>
+							</label>
+							<input
+								type="file"
+								name="banner"
 								accept="image/png, image/jpg, image/jpeg, image/svg"
 								className="mt-3 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
 								onChange={handleFileChange}
@@ -188,13 +270,15 @@ const Profile = () => {
 										Email
 									</label>
 									<p className="w-full mt-2 px-3 py-2 text-black bg-[darkgrey] outline-none border-2 border-[darkgrey] shadow-sm rounded-lg">
-										{credentials.email || "example@mail.com"}
+										{credentials.email ||
+											"example@mail.com"}
 									</p>
 								</div>
 
-								<div className="md:col-span-3">
+								<div className="md:col-span-5">
 									<label className="text-sm text-gray-600 font-bold">
-										Bio
+										Bio{" "}
+										<span className="text-red-500">*</span>
 									</label>
 									<textarea
 										name="bio"
@@ -205,6 +289,7 @@ const Profile = () => {
 										onChange={onChange}
 										minLength={10}
 										maxLength={300}
+										required
 									/>
 								</div>
 
@@ -212,11 +297,11 @@ const Profile = () => {
 									<label className="text-sm text-gray-600 font-bold">
 										Change password
 									</label>
-									<section className="flex gap-2">
+									<section className="sm:flex gap-2">
 										<input
 											type="password"
 											name="oldPassword"
-											className="mt-2 w-full px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-[#3a8358] shadow-sm rounded-lg"
+											className="mt-2 w-full px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-indigo-600 shadow-sm rounded-lg"
 											value={credentials.oldPassword}
 											onChange={onChange}
 											placeholder="Enter old password"
@@ -226,7 +311,7 @@ const Profile = () => {
 										<input
 											type="password"
 											name="newPassword"
-											className="mt-2 w-full px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-[#3a8358] shadow-sm rounded-lg"
+											className="mt-2 w-full px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-indigo-600 shadow-sm rounded-lg"
 											value={credentials.newPassword}
 											onChange={onChange}
 											placeholder="Enter new password"
@@ -236,7 +321,7 @@ const Profile = () => {
 										<input
 											type="password"
 											name="confirmNewPassword"
-											className="mt-2 w-full px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-[#3a8358] shadow-sm rounded-lg"
+											className="mt-2 w-full px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-indigo-600 shadow-sm rounded-lg"
 											value={
 												credentials.confirmNewPassword
 											}
@@ -265,6 +350,24 @@ const Profile = () => {
 									/>
 								</div>
 
+								<div className="md:col-span-3">
+									<label className="text-sm text-gray-600 font-bold">
+										Address{" "}
+										<span className="text-red-500">*</span>
+									</label>
+									<textarea
+										name="address"
+										className="w-full mt-2 px-3 py-2 text-black bg-[darkgrey] outline-none border-2 border-[darkgrey] shadow-sm rounded-lg"
+										placeholder="Enter address"
+										rows={1}
+										// value={credentials.address}
+										// onChange={onChange}
+										minLength={10}
+										maxLength={300}
+										disabled
+									/>
+								</div>
+
 								<div className="md:col-span-2">
 									<label className="text-sm text-gray-600 font-bold">
 										Display Email
@@ -272,7 +375,7 @@ const Profile = () => {
 									<select
 										className="w-full mt-2 px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-indigo-600 shadow-sm rounded-lg"
 										name="displayEmail"
-										defaultValue={credentials.displayEmail}
+										value={credentials.displayEmail}
 										onChange={(e) => {
 											setCredentials({
 												...credentials,
@@ -294,7 +397,7 @@ const Profile = () => {
 									<select
 										className="w-full mt-2 px-3 py-2 text-black bg-transparent outline-none border-2 border-[darkgrey] focus:border-indigo-600 shadow-sm rounded-lg"
 										name="displayPhone"
-										defaultValue={credentials.displayPhone}
+										value={credentials.displayPhone}
 										onChange={(e) => {
 											setCredentials({
 												...credentials,
